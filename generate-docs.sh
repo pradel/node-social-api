46582072ad8feda6ab4b3ee78eb0705b9264c0d3
Pradel Léo <pradel.leo@gmail.com>
2016-09-08 15:53:06 +0200

    Fix doc generation

--- generate-docs.sh
+++ generate-docs.sh
@@ -4,9 +4,9 @@ cd node-social-api
 npm i
 npm run build:docs
 mv docs/* ../
+cd ..
 version=$(node -p "require('./node-social-api/package.json').version" 2>/dev/null) ||
   (echo "Cannot read package version" >&2 ; exit 1)
-cd ..
 rm -rf node-social-api
 git add .
 git commit --message '$version'
