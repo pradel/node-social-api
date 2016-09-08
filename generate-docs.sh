b7dc15afbb22b84619c039f5aff5fc77f4a300af
Pradel Léo <pradel.leo@gmail.com>
2016-09-08 15:51:54 +0200

    $version

---
+++ generate-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://github.com/pradel/node-social-api
+cd node-social-api
+npm i
+npm run build:docs
+mv docs/* ../
+version=$(node -p "require('./node-social-api/package.json').version" 2>/dev/null) ||
+  (echo "Cannot read package version" >&2 ; exit 1)
+cd ..
+rm -rf node-social-api
+git add .
+git commit --message '$version'
+git push origin gh-pages
