a401b05aa9b4b1dd687faa073e4dfa5455c002a2
Pradel Léo <pradel.leo@gmail.com>
2016-09-08 16:14:15 +0200

    Add gitignore

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 git clone https://github.com/pradel/node-social-api
-cd node-social-api
-npm i
-npm run build:docs
-mv docs/* ../
-cd ..
+mv node-social-api/.gitignore .
+mv docs/* .
 version=$(node -p "require('./node-social-api/package.json').version" 2>/dev/null) ||
   (echo "Cannot read package version" >&2 ; exit 1)
 rm -rf node-social-api
